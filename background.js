a020a47d57bd6388ea108ee44007384d6741f490
Manton Reece <manton@me.com>
2024-02-04 09:34:34 -0600

    Updated to manifest version 3.

--- background.js
+++ background.js
@@ -1,12 +1,21 @@
-function saveBookmark() {
-	browser.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-		let tab = tabs[0];
-		let url = tab.url;
-
-		browser.tabs.create({
-			url: "https://micro.blog/bookmarks?go=" + encodeURIComponent(url)
-		});
-	});		
+function saveBookmark(tab) {
+// 	browser.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+// 		let tab = tabs[0];
+// 		let url = tab.url;
+// 
+// 		browser.tabs.create({
+// 			url: "https://micro.blog/bookmarks?go=" + encodeURIComponent(url)
+// 		});
+// 	});
+	
+	let url= tab.url;
+	chrome.tabs.create({
+		url: "https://micro.blog/bookmarks?go=" + encodeURIComponent(url)
+	});	
 }
 
-browser.browserAction.onClicked.addListener(saveBookmark);
+// browser.browserAction.onClicked.addListener(saveBookmark);
+
+chrome.action.onClicked.addListener((tab) => {
+	saveBookmark(tab);
+});
